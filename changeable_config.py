41036969e773ca5ea74169d63f5c442160fcf51b
kooflixy <kooflixy@gmail.com>
2025-06-05 14:28:43 +0300

    Add changeable settings

---
+++ changeable_config.py
@@ -0,0 +1,36 @@
+from typing import Literal
+from pydantic import BaseModel, field_serializer, field_validator
+
+from db.data_manager import JSONManager
+from db.models import ActionTypeEnum
+
+CHANGEABLE_SETTINGS_PATH = 'db/data/data.json'
+
+
+class ChangeableSettings(BaseModel):
+    '''Настройки, которые могут меняться пользователем в течение программы
+    После каждого изменения следует сохранять изменения в CHANGEABLE_SETTINGS_PATH с помощью функции ChangeableSettings.commit()'''
+    captcha_text: str = "Привет, {user}! Нажми на кнопку, чтобы подтвердить, что ты не робот."
+    captcha_waitng: int = 60
+
+    max_warn_restriction: Literal[ActionTypeEnum.MUTE, ActionTypeEnum.BAN] = ActionTypeEnum.MUTE
+    max_watn_count: int = 3
+
+    remove_system_messages_waiting: int = 30
+
+    remove_ban_notifications: int = 120
+
+    @field_validator('max_warn_restriction', mode='before')
+    def validate_max_warn_restriction(v: str):
+        return ActionTypeEnum._value2member_map_[v]
+
+    @field_serializer('max_warn_restriction')
+    def serialize_max_warn_restriction(self, max_warn_restriction: ActionTypeEnum, _info):
+        return max_warn_restriction.value
+
+    def commit(self):
+        self.model_dump_json(indent=4)
+        JSONManager.insert_json(CHANGEABLE_SETTINGS_PATH, self.model_dump(mode='python'))
+
+data = JSONManager.get_json(CHANGEABLE_SETTINGS_PATH)
+changeable_settings = ChangeableSettings(**data)
---
+++ db/data_manager.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+import orjson
+
+
+class JSONManager:
+    @staticmethod
+    def get_json(path) -> dict:
+        if os.path.exists(path):
+            with open(path, 'r', encoding='utf-8') as file:
+                data = orjson.loads(file.read())
+            return data
+        return {}
+    
+    @staticmethod
+    def insert_json(path, obj) -> None:
+        with open(path, 'w', encoding='utf-8') as file:
+            json.dump(obj, file, indent=4, ensure_ascii=False)
