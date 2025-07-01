fbc1f1f26fe84097907a0c1989e29baa49f9b097
kooflixy <kooflixy@gmail.com>
2025-07-01 18:13:46 +0300

    импорты в файлах рассылки

--- app/handlers/settings/distribution_settings/distribution.py
+++ app/handlers/settings/distribution_settings/distribution.py
@@ -1,37 +1,26 @@
 from datetime import timedelta
 from logging import getLogger
 
-from aiogram import F, Router
+from aiogram import Router
 from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters import Command, CommandObject
-from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, Message
 
 from app.bot_obj import bot
-from app.handlers.user_commands import settings_cmd
-from app.keyboards.settings.captcha import *
 from app.keyboards.settings.distribution import (
-    AddDistributionChatCD,
-    AddDistributionChatListCD,
     ChangeDistributionActivityCD,
-    DistributionChatListCD,
     DistributionDetailsCD,
     RemoveDistributionCD,
-    RemoveDistributionChatCD,
     ShowDistributionCD,
-    add_distribution_chat_list_ikb,
-    distribution_chat_list_ikb,
     distribution_details_ikb,
     distribution_list_ikb,
 )
 from app.keyboards.settings_menu import DistributionListCD
-from app.utils.answer_templates import error_cb_ans
 from app.utils.contrib import time_text_to_seconds
 from app.utils.for_logging import name_in_log
 from config import settings
 from db.database import async_session_factory
 from db.models import DistributionORM
-from db.queries.distribution_chat_orm import DistributionChatORMHandler
 from db.queries.distribution_orm import DistributionORMHandler
 
 log = getLogger(__name__)
