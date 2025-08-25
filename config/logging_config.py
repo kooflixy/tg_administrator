6ab157e6e059557e090295160e1fbb9758b5664f
kooflixy <kooflixy@gmail.com>
2025-08-25 17:20:03 +0300

    logging configure

--- config/logging_config.py
+++ config/logging_config.py
@@ -7,13 +7,31 @@ def logging_configure(level=logging.INFO, filemode: Literal["a", "w"] = "a"):
         logging.WARNING
     )  # логи aiogram только с logging.WARNING
 
-    logging.getLogger("telethon").setLevel(logging.WARNING)
 
-    logging.basicConfig(
-        level=level,
-        filename="logs.log",
-        filemode=filemode,
-        encoding="utf-8",
+    # Создание корневого логгера
+    logger = logging.getLogger()
+    logger.setLevel(level)
+
+    # Файловый хендлер
+    file_formatter = logging.Formatter(
+        fmt="[%(asctime)s.%(msecs)03d] %(name)70s:%(lineno)-3s %(levelname)-7s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
-        format="[%(asctime)s.%(msecs)03d] %(name)70s:%(lineno)-3s %(levelname)-7s - %(message)s",
     )
+
+    file_handler = logging.FileHandler("logs.log", mode=filemode, encoding="utf-8")
+    file_handler.setFormatter(file_formatter)
+    file_handler.setLevel(level)
+
+    # Консольный хендлер
+    console_formatter = logging.Formatter(
+        fmt="[%(asctime)s.%(msecs)03d] %(levelname)-7s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(console_formatter)
+    console_handler.setLevel(level)
+
+    # Добавляем хендлеры
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
